b82431ce503dd7da21d3038b0f7c4abd41a833aa
TheWallovec <matuswall@gmail.com>
2020-08-14 22:57:40 +0200

    idk

--- server.js
+++ server.js
@@ -25,22 +25,24 @@ function copyObj(object) {
     return result
 }
 
-function sendableGamesList(){
+function sendableGamesList() {
     result = {}
-    for(const index in gamesList){
-        result[index] = {}
-        result[index].player1 = gamesList[index].player1
-        result[index].player2 = gamesList[index].player2
-        result[index].password = gamesList[index].password == "" ? "no" : "yes"
-        result[index].name = gamesList[index].name
+    for (const index in gamesList) {
+        if (gamesList[index] != undefined) {
+            result[index] = {}
+            result[index].player1 = gamesList[index].player1
+            result[index].player2 = gamesList[index].player2
+            result[index].password = gamesList[index].password == "" ? "no" : "yes"
+            result[index].name = gamesList[index].name
+        }
     }
     return result
 }
 
-function removeTimers(object){
+function removeTimers(object) {
     result = {}
-    for(const x in object){
-        if(object[x]._idlePrev == undefined){
+    for (const x in object) {
+        if (object[x]._idlePrev == undefined) {
             result[x] = object[x]
         }
     }
@@ -50,9 +52,9 @@ function removeTimers(object){
 function filterAlive(array) {
     result = []
     for (x = 0; x < array.length; x++) {
-      if (array[x].alive) {
-        result.push(array[x])
-      }
+        if (array[x].alive) {
+            result.push(array[x])
+        }
     }
     return result;
 }
@@ -62,13 +64,13 @@ function filterAlive(array) {
 */
 function collidesWithX(obj1, obj2) {
     if (Math.abs(obj1.xpos - obj2.xpos) < Math.abs(obj1.width - obj2.width)) {
-      return true;
+        return true;
     } else return false
 }
 
 function collidesWithY(obj1, obj2) {
     if (Math.abs(obj1.ypos - (700 - obj2.ypos)) < (Math.abs(obj1.height - obj2.height) + Math.min(obj1.height, obj2.height))) {
-      return true;
+        return true;
     } else return false
 }
 
@@ -83,15 +85,12 @@ function collidesWith(obj1, obj2) {
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-    req.testing = 'testing';
-    return next();
-  })
 
-app.ws("/", function(ws, req){
-    ws.on("message", function(msg){
+app.ws("/", function (ws, req) {
+    ws.on("message", function (msg) {
         input = JSON.parse(msg)
-        switch(input.request){
+        //console.log(input)
+        switch (input.request) {
             case "login":
                 connectedPlayers.push(input.playerName)
                 ws.send(JSON.stringify({
@@ -120,8 +119,7 @@ app.ws("/", function(ws, req){
                 }
                 break
             case "joinGame":
-                console.log(gamesList[input.name])
-                if(input.password == gamesList[input.name].password){
+                if (input.password == gamesList[input.name].password) {
                     runningGamesList[input.name] = copyObj(gamesList[input.name])
                     gamesList[input.name] = undefined
                     runningGamesList[input.name].player2 = input.playerName
@@ -129,13 +127,14 @@ app.ws("/", function(ws, req){
                     runningGamesList[input.name].player2ready = false
                     runningGamesList[input.name].gameStarted = false
                     runningGamesList[input.name].gameFinished = false
-                    if(runningGamesList[input.name]){
+                    if (runningGamesList[input.name]) {
                         runningGamesList[input.name].lobbyTimer = setInterval((name) => {
-                            if(runningGamesList[name].player1ready && runningGamesList[name].player2ready){
+                            if (runningGamesList[name].player1ready && runningGamesList[name].player2ready) {
                                 clearInterval(runningGamesList[name].lobbyTimer)
                                 runningGamesList[name].gameStarted = true
-                                runningGamesList[name].gameTimer = setInterval(game, 1000/60, runningGamesList[name]);
+                                runningGamesList[name].gameTimer = setInterval(game, 1000 / 60, runningGamesList[name]);
                                 runningGamesList[name].player1 = {
+                                    scope: true,
                                     weaponDamage: 10,
                                     soldierSpawnCooldown: 0,
                                     health: 100,
@@ -155,6 +154,7 @@ app.ws("/", function(ws, req){
                                     xpos: 350
                                 }
                                 runningGamesList[name].player2 = {
+                                    scope: true,
                                     weaponDamage: 10,
                                     soldierSpawnCooldown: 0,
                                     health: 100,
@@ -182,27 +182,27 @@ app.ws("/", function(ws, req){
                 }
                 break
             case "checkGameStatus":
-                if(gamesList[input.gameName] == undefined){
+                if (gamesList[input.gameName] == undefined) {
                     temp = removeTimers(runningGamesList[input.gameName])
                     temp.request = "checkGameStatus"
                     ws.send(JSON.stringify(temp))
-                } else{
+                } else {
                     temp = gamesList[input.gameName]
                     temp.request = "checkGameStatus"
                     ws.send(JSON.stringify(temp))
                 }
                 break
             case "readyCheck":
-                if(input.player == 1){
-                    if(runningGamesList[input.name].player1ready){
+                if (input.player == 1) {
+                    if (runningGamesList[input.name].player1ready) {
                         runningGamesList[input.name].player1ready = false
-                    }else{
+                    } else {
                         runningGamesList[input.name].player1ready = true
                     }
-                }else{
-                    if(runningGamesList[input.name].player2ready){
+                } else {
+                    if (runningGamesList[input.name].player2ready) {
                         runningGamesList[input.name].player2ready = false
-                    }else{
+                    } else {
                         runningGamesList[input.name].player2ready = true
                     }
                 }
@@ -211,7 +211,7 @@ app.ws("/", function(ws, req){
                 ws.send(JSON.stringify(temp))
                 break
             case "game":
-                if(input.player == 1){
+                if (input.player == 1) {
                     runningGamesList[input.name].player1.left = input.left
                     runningGamesList[input.name].player1.up = input.up
                     runningGamesList[input.name].player1.right = input.right
@@ -219,7 +219,7 @@ app.ws("/", function(ws, req){
                     runningGamesList[input.name].player1.space = input.space
                     runningGamesList[input.name].player1.shift = input.shift
                     runningGamesList[input.name].player1.ctrl = input.ctrl
-                } else{
+                } else {
                     runningGamesList[input.name].player2.left = input.left
                     runningGamesList[input.name].player2.up = input.up
                     runningGamesList[input.name].player2.right = input.right
@@ -233,8 +233,8 @@ app.ws("/", function(ws, req){
                 ws.send(JSON.stringify(temp))
                 break
             case "yellowSoldierSpawn":
-                if(input.player == 1){
-                    if(runningGamesList[input.name].player1.money >= 10 && runningGamesList[input.name].player1.soldierSpawnCooldown == 0){
+                if (input.player == 1) {
+                    if (runningGamesList[input.name].player1.money >= 10 && runningGamesList[input.name].player1.soldierSpawnCooldown == 0) {
                         runningGamesList[input.name].player1.soldiers.push({
                             type: "yellow",
                             health: 10,
@@ -247,8 +247,8 @@ app.ws("/", function(ws, req){
                         runningGamesList[input.name].player1.money -= 10
                         runningGamesList[input.name].player1.soldierSpawnCooldown = 60
                     }
-                }else{
-                    if(runningGamesList[input.name].player2.money >= 10 && runningGamesList[input.name].player2.soldierSpawnCooldown == 0){
+                } else {
+                    if (runningGamesList[input.name].player2.money >= 10 && runningGamesList[input.name].player2.soldierSpawnCooldown == 0) {
                         runningGamesList[input.name].player2.soldiers.push({
                             type: "yellow",
                             health: 10,
@@ -264,8 +264,8 @@ app.ws("/", function(ws, req){
                 }
                 break
             case "blueSoldierSpawn":
-                if(input.player == 1){
-                    if(runningGamesList[input.name].player1.money >= 20 && runningGamesList[input.name].player1.soldierSpawnCooldown == 0){
+                if (input.player == 1) {
+                    if (runningGamesList[input.name].player1.money >= 20 && runningGamesList[input.name].player1.soldierSpawnCooldown == 0) {
                         runningGamesList[input.name].player1.soldiers.push({
                             type: "blue",
                             health: 15,
@@ -278,8 +278,8 @@ app.ws("/", function(ws, req){
                         runningGamesList[input.name].player1.money -= 20
                         runningGamesList[input.name].player1.soldierSpawnCooldown = 90
                     }
-                }else{
-                    if(runningGamesList[input.name].player2.money >= 20 && runningGamesList[input.name].player2.soldierSpawnCooldown == 0){
+                } else {
+                    if (runningGamesList[input.name].player2.money >= 20 && runningGamesList[input.name].player2.soldierSpawnCooldown == 0) {
                         runningGamesList[input.name].player2.soldiers.push({
                             type: "blue",
                             health: 15,
@@ -293,7 +293,7 @@ app.ws("/", function(ws, req){
                         runningGamesList[input.name].player2.soldierSpawnCooldown = 60
                     }
                 }
-                
+
         }
     })
     ws.on("close", () => {
@@ -306,88 +306,86 @@ app.get("/", function (req, res) {
 })
 
 server = app.listen(PORT, function () {
-    console.log("Server is running on port " +  PORT + ". Hit CTRL+C to stop.");
+    console.log("Server is running on port " + PORT + ". Hit CTRL+C to stop.");
 })
 
 /*
 ---GAME CODE---
 */
 
-function game(game){
+function game(game) {
     /*
     ---PLAYER MOVEMENT---
     */
-    if(game.gameFinished) game = undefined
-    if(game.player1.left && game.player1.xpos >= 0){
-        if(game.player1.shift && game.player1.ctrl || !game.player1.shift && !game.player1.ctrl) game.player1.xpos -= 6
-        if(game.player1.shift && !game.player1.ctrl) game.player1.xpos -= 12
-        if(!game.player1.shift && game.player1.ctrl) game.player1.xpos -= 3
-    } 
-    if(game.player1.right && game.player1.xpos <= 700){
-        if(game.player1.shift && game.player1.ctrl || !game.player1.shift && !game.player1.ctrl) game.player1.xpos += 6
-        if(game.player1.shift && !game.player1.ctrl) game.player1.xpos += 12
-        if(!game.player1.shift && game.player1.ctrl) game.player1.xpos += 3
-    } 
+    if (game.gameFinished) game = undefined
+    if (game.player1.left && game.player1.xpos >= 0) {
+        if (game.player1.shift && game.player1.ctrl || !game.player1.shift && !game.player1.ctrl) game.player1.xpos -= 6
+        if (game.player1.shift && !game.player1.ctrl) game.player1.xpos -= 12
+        if (!game.player1.shift && game.player1.ctrl) game.player1.xpos -= 3
+    }
+    if (game.player1.right && game.player1.xpos <= 700) {
+        if (game.player1.shift && game.player1.ctrl || !game.player1.shift && !game.player1.ctrl) game.player1.xpos += 6
+        if (game.player1.shift && !game.player1.ctrl) game.player1.xpos += 12
+        if (!game.player1.shift && game.player1.ctrl) game.player1.xpos += 3
+    }
 
-    if(game.player2.left && game.player2.xpos >= 0){
-        if(game.player2.shift && game.player2.ctrl || !game.player2.shift && !game.player2.ctrl) game.player2.xpos -= 6
-        if(game.player2.shift && !game.player2.ctrl) game.player2.xpos -= 12
-        if(!game.player2.shift && game.player2.ctrl) game.player2.xpos -= 3
-    } 
-    if(game.player2.right && game.player2.xpos <= 700){
-        if(game.player2.shift && game.player2.ctrl || !game.player2.shift && !game.player2.ctrl) game.player2.xpos += 6
-        if(game.player2.shift && !game.player2.ctrl) game.player2.xpos += 12
-        if(!game.player2.shift && game.player2.ctrl) game.player2.xpos += 3
-    } 
+    if (game.player2.left && game.player2.xpos >= 0) {
+        if (game.player2.shift && game.player2.ctrl || !game.player2.shift && !game.player2.ctrl) game.player2.xpos -= 6
+        if (game.player2.shift && !game.player2.ctrl) game.player2.xpos -= 12
+        if (!game.player2.shift && game.player2.ctrl) game.player2.xpos -= 3
+    }
+    if (game.player2.right && game.player2.xpos <= 700) {
+        if (game.player2.shift && game.player2.ctrl || !game.player2.shift && !game.player2.ctrl) game.player2.xpos += 6
+        if (game.player2.shift && !game.player2.ctrl) game.player2.xpos += 12
+        if (!game.player2.shift && game.player2.ctrl) game.player2.xpos += 3
+    }
     /*
     ---SHOOTING---
     */
-    if(game.player1.space && game.player1.shootCd == 0){
+    if (game.player1.space && game.player1.shootCd == 0) {
         game.player1.shots.push({
             xpos: game.player1.xpos,
             ypos: 650,
             width: game.player1.bulletWidth,
             height: 20,
-            id: game.player1.shots.length,
             alive: true
-          })
+        })
         game.player1.shootCd = game.player1.shootRechargeTime * 60
     }
-    if(game.player2.space && game.player2.shootCd == 0){
+    if (game.player2.space && game.player2.shootCd == 0) {
         game.player2.shots.push({
             xpos: game.player2.xpos,
             ypos: 650,
             width: game.player2.bulletWidth,
             height: 20,
-            id: game.player2.shots.length,
             alive: true
-          })
+        })
         game.player2.shootCd = game.player2.shootRechargeTime * 60
     }
     /*
     ---SHOT MOVEMENT---
     */
-    for(x of game.player1.shots){
+    for (x of game.player1.shots) {
         x.ypos -= 10
     }
-    for(x of game.player2.shots){
+    for (x of game.player2.shots) {
         x.ypos -= 10
     }
     /*
     ---COOLDOWN RECOVERY---
     */
-    if(game.player1.shootCd > 0) game.player1.shootCd--
-    if(game.player2.shootCd > 0) game.player2.shootCd--
-    if(game.player1.soldierSpawnCooldown > 0) game.player1.soldierSpawnCooldown--
-    if(game.player2.soldierSpawnCooldown > 0) game.player2.soldierSpawnCooldown--
+    if (game.player1.shootCd > 0) game.player1.shootCd--
+    if (game.player2.shootCd > 0) game.player2.shootCd--
+    if (game.player1.soldierSpawnCooldown > 0) game.player1.soldierSpawnCooldown--
+    if (game.player2.soldierSpawnCooldown > 0) game.player2.soldierSpawnCooldown--
     /*
     ---SHOTS CLEANUP---
     */
-    for(x of game.player1.shots){
-        if(x.ypos < 0) x.alive = false //IF A SHOT EXITS THE MAP
+    for (x of game.player1.shots) {
+        if (x.ypos < 0) x.alive = false //IF A SHOT EXITS THE MAP
     }
-    for(x of game.player2.shots){
-        if(x.ypos < 0) x.alive = false
+    for (x of game.player2.shots) {
+        if (x.ypos < 0) x.alive = false
     }
 
     game.player1.shots = filterAlive(game.player1.shots)
@@ -395,13 +393,13 @@ function game(game){
     /*
     ---PASSIVE MONEY GAIN---
     */
-    game.player1.money += 1/30
-    game.player2.money += 1/30
+    game.player1.money += 1 / 30
+    game.player2.money += 1 / 30
     /*
     ---SOLDIER MOVEMENT---
     */
-    for(x of game.player1.soldiers){
-        switch(x.type){
+    for (x of game.player1.soldiers) {
+        switch (x.type) {
             case "yellow":
                 x.ypos -= 3
                 break
@@ -409,8 +407,8 @@ function game(game){
                 x.ypos -= 2
         }
     }
-    for(x of game.player2.soldiers){
-        switch(x.type){
+    for (x of game.player2.soldiers) {
+        switch (x.type) {
             case "yellow":
                 x.ypos -= 3
                 break
@@ -421,14 +419,14 @@ function game(game){
     /*
     ---SOLDIER CLEANUP---
     */
-    for(x of game.player1.soldiers){
-        if(x.ypos < 0) {
-            x.alive = false 
+    for (x of game.player1.soldiers) {
+        if (x.ypos < 0) {
+            x.alive = false
             game.player2.health -= x.health
         }
     }
-    for(x of game.player2.soldiers){
-        if(x.ypos < 0) {
+    for (x of game.player2.soldiers) {
+        if (x.ypos < 0) {
             x.alive = false
             game.player1.health -= x.health
         }
@@ -438,28 +436,28 @@ function game(game){
     /*
     ---COLLISIONS---
     */
-    for(i of game.player1.soldiers){
-        for(ii of game.player2.shots){
-            if(collidesWith(i,ii)){
-                if(i.health <= game.player2.weaponDamage){
+    for (i of game.player1.soldiers) {
+        for (ii of game.player2.shots) {
+            if (collidesWith(i, ii)) {
+                if (i.health <= game.player2.weaponDamage) {
                     i.alive = false
                     ii.alive = false
                     game.player2.money += 10
-                }else{
+                } else {
                     i.health -= game.player2.weaponDamage
                     ii.alive = false
                 }
             }
         }
     }
-    for(i of game.player2.soldiers){
-        for(ii of game.player1.shots){
-            if(collidesWith(i,ii)){
-                if(i.health <= game.player1.weaponDamage){
+    for (i of game.player2.soldiers) {
+        for (ii of game.player1.shots) {
+            if (collidesWith(i, ii)) {
+                if (i.health <= game.player1.weaponDamage) {
                     i.alive = false
                     ii.alive = false
                     game.player1.money += 10
-                }else{
+                } else {
                     i.health -= game.player1.weaponDamage
                     ii.alive = false
                 }
