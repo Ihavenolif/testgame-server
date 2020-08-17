57eefa55de5dfd1d258309ec5327f441e0db6ef5
TheWallovec <matuswall@gmail.com>
2020-08-17 20:40:26 +0200

    created collision.js

---
+++ modules/collision.js
@@ -0,0 +1,15 @@
+exports.collidesWithX = (obj1, obj2) => {
+    if (Math.abs(obj1.xpos - obj2.xpos) < Math.abs(obj1.width - obj2.width)) {
+        return true
+    } else return false
+}
+
+exports.collidesWithY = (obj1, obj2) => {
+    if (Math.abs(obj1.ypos - (700 - obj2.ypos)) < (Math.abs(obj1.height - obj2.height) + Math.min(obj1.height, obj2.height))) {
+        return true
+    } else return false
+}
+
+exports.collidesWithX = (obj1, obj2) => {
+    return exports.collidesWithX(obj1, obj2) && exports.collidesWithY(obj1, obj2)
+}
